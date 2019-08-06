5dfd5d62f5d661e64d3343375cb47998537d96d7
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-08-06 15:46:10 +0900

    Sokovan: player key input

--- Sokovan/Assets/Scenes/Player.cs
+++ Sokovan/Assets/Scenes/Player.cs
@@ -7,42 +7,31 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
     public float speed = 10f;
-    public Rigidbody playerRigidbody;
+    private Rigidbody playerRigidbody;
 
 	// Use this for initialization
 	void Start () {
-        
+        // GetComponent -> search <?> in game component
+        playerRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
         // user input
-        /*
-        if (Input.GetKey(KeyCode.W))
-        {
-            playerRigidbody.AddForce(0, 0, speed);
-        }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            playerRigidbody.AddForce(-speed, 0, 0);
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            playerRigidbody.AddForce(0, 0, -speed);
-        }
-
-        if (Input.GetKey(KeyCode.D))
-        {
-            playerRigidbody.AddForce(speed, 0, 0);
-        }
-        */
-
         /* <- or A : return -1   
          -> or D : return 1  
          press noting : return 0 */    
         float inputX = Input.GetAxis("Horizontal");     //"Horizontal" -> horizontal key mapped (<-/->, )
+        float inputZ = Input.GetAxis("Vertical");     //"Horizontal" -> horizontal key mapped (<-/->, )
+
+        float fallSpeed = playerRigidbody.velocity.y;
+
+        Vector3 velocity = new Vector3(inputX, 0, inputZ);
+        velocity *= speed;
+        velocity.y = fallSpeed;
+
+        playerRigidbody.velocity = velocity;
+
     }
 }
