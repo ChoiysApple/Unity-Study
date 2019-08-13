c3d82cf7682a9488d47cb5cc55ba0d9eadd3db4c
Daegun Choi <43776784+ChoiysApple@users.noreply.github.com>
2019-08-13 10:42:53 +0900

    UI, Restart function

---
+++ Sokovan/Assets/Scenes/Script/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+
+    public ItemBox[] itemBoxes;
+    public GameObject winUI;
+
+    public bool isGameOver;
+	// Use this for initialization
+	void Start () {
+        winUI.SetActive(false);
+        isGameOver = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+
+        if (isGameOver)
+        {
+            winUI.SetActive(true);
+            return;
+        }
+
+        int count = 0;
+        for(int i = 0; i < 3; i++)
+        {
+            if (itemBoxes[i].isOverlaped)
+            {
+                count++;
+            }
+        }
+
+        if (count == 3)
+        {
+            Debug.Log("Victory");
+            isGameOver = true;
+        }
+	}
+}
---
+++ Sokovan/Assets/Scenes/Script/ItemBox.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemBox : MonoBehaviour {
+
+
+    public bool isOverlaped = false;
+
+
+    private Renderer myRenderer;
+
+    public Color touchColor;
+    private Color originalColor;
+	// Use this for initialization
+	void Start () {
+        myRenderer = GetComponent<Renderer>();
+        originalColor = myRenderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Enter: the monent of collison
+    // Unity launch this function automatically, Broadcast OnTrigger Message
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "EndPoint") {
+            isOverlaped = true;
+            myRenderer.material.color = touchColor;
+        }
+       
+    }
+    
+    // when collion ended
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "EndPoint")
+        {
+            isOverlaped = false;
+            myRenderer.material.color = originalColor;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.tag == "Endpoint")
+        {
+            isOverlaped = true;
+            myRenderer.material.color = touchColor;
+        }
+    }
+
+}
